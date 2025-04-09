19b6d837561cbe4662524064888c3e9861fdf3db
Ujae <ujae88@gmail.com>
2025-04-09 17:26:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import LatinSquareEditor from './LatinSquareEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <LatinSquareEditor />
+    </div>
+  );
+}
+
+export default App;
